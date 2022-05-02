d7d6e16ecd4a10e4725f4fe5a2aa1b915ba1e905
user <user@DESKTOP-6JBEF5K>
2022-05-02 11:26:11 +0800

    TCP Client

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_sendAll = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox_OnlineList = new System.Windows.Forms.ListBox();
+            this.textBox_main = new System.Windows.Forms.TextBox();
+            this.textBox_sendMsg = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "伺服器IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "玩家名稱";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "伺服器Port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(265, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(69, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "登入伺服器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_sendAll
+            // 
+            this.button_sendAll.Location = new System.Drawing.Point(490, 14);
+            this.button_sendAll.Name = "button_sendAll";
+            this.button_sendAll.Size = new System.Drawing.Size(113, 23);
+            this.button_sendAll.TabIndex = 7;
+            this.button_sendAll.Text = "廣播";
+            this.button_sendAll.UseVisualStyleBackColor = true;
+            this.button_sendAll.Click += new System.EventHandler(this.button_sendAll_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label4.Location = new System.Drawing.Point(504, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "線上使用者";
+            // 
+            // listBox_OnlineList
+            // 
+            this.listBox_OnlineList.FormattingEnabled = true;
+            this.listBox_OnlineList.ItemHeight = 12;
+            this.listBox_OnlineList.Location = new System.Drawing.Point(478, 98);
+            this.listBox_OnlineList.Name = "listBox_OnlineList";
+            this.listBox_OnlineList.Size = new System.Drawing.Size(132, 244);
+            this.listBox_OnlineList.TabIndex = 9;
+            // 
+            // textBox_main
+            // 
+            this.textBox_main.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox_main.Location = new System.Drawing.Point(69, 98);
+            this.textBox_main.Multiline = true;
+            this.textBox_main.Name = "textBox_main";
+            this.textBox_main.Size = new System.Drawing.Size(385, 244);
+            this.textBox_main.TabIndex = 10;
+            // 
+            // textBox_sendMsg
+            // 
+            this.textBox_sendMsg.Location = new System.Drawing.Point(121, 378);
+            this.textBox_sendMsg.Name = "textBox_sendMsg";
+            this.textBox_sendMsg.Size = new System.Drawing.Size(333, 22);
+            this.textBox_sendMsg.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label5.Location = new System.Drawing.Point(27, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "發言內容";
+            // 
+            // button_Send
+            // 
+            this.button_Send.Enabled = false;
+            this.button_Send.Location = new System.Drawing.Point(479, 376);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(113, 23);
+            this.button_Send.TabIndex = 13;
+            this.button_Send.Text = "送出";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 428);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_sendMsg);
+            this.Controls.Add(this.textBox_main);
+            this.Controls.Add(this.listBox_OnlineList);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_sendAll);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TCP 客戶端";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_sendAll;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox_OnlineList;
+        private System.Windows.Forms.TextBox textBox_main;
+        private System.Windows.Forms.TextBox textBox_sendMsg;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_Send;
+    }
+}
+
